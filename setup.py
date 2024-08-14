de8bfb35f274369b1c8fecd130a6e9d152f050f5
ludobrands <ludobrands@users.noreply.github.com>
2024-08-14 14:45:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='async-hvac-fork',
     version='0.6.1',
     description='HashiCorp Vault API client, forked from Aloomaio/async-hvac',
-    long_description='HashiCorp Vault API python 3.8+ client using asyncio. Supports aiohttp==3.8.1',
+    long_description='HashiCorp Vault API python 3.8+ client using asyncio. Supports aiohttp==3.8.4',
     author='northpowered',
     author_email='',
     url='https://github.com/northpowered/async-hvac',
@@ -13,7 +13,7 @@ setup(
     classifiers=['License :: OSI Approved :: Apache Software License'],
     packages=find_packages(),
     install_requires=[
-        'aiohttp==3.8.1',
+        'aiohttp==3.8.4',
     ],
     include_package_data=True,
     package_data={'async_hvac': ['version']},
